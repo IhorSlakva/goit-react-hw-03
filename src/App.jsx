6fdd06b2997a25f6a63b8bd2af678a1a35d7dda7
IhorSlakva <igorek.slakva@gmail.com>
2024-03-22 22:43:22 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
-import contactsTest from "./contactsTest.json";
+import initialContacts from "./initialContacts.json";
 import ContactList from "./components/ContactList/ContactList";
 import ContactForm from "./components/ContactForm/ContactForm";
 import SearchBox from "./components/SearchBox/SearchBox";
+import { useState } from "react";
+import { nanoid } from "nanoid";
 
 function App() {
+  const [contacts, setContacts] = useState(initialContacts);
+  const [filter, setFilter] = useState("");
+
+  const filteredContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  const addContact = (newContact) => {
+    const contact = {
+      ...newContact,
+      id: nanoid(),
+    };
+    setContacts((prevState) => [...prevState, contact]);
+  };
+
   return (
     <>
       <h1>Phonebook</h1>
-      <ContactForm />
-      <SearchBox />
-      <ContactList contacts={contactsTest} />
+      <ContactForm onAddContact={addContact} />
+      <SearchBox value={filter} onFilter={setFilter} />
+      <ContactList contacts={filteredContacts} />
     </>
   );
 }
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,5 +1,64 @@
-const ContactForm = () => {
-  return <div>ContactForm</div>;
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { useId } from "react";
+import * as Yup from "yup";
+import css from "./ContactForm.module.css";
+
+const ContactFormSchema = Yup.object().shape({
+  username: Yup.string()
+    .min(3, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+  usernumber: Yup.string()
+    .min(3, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+});
+
+const initialValues = {
+  username: "",
+  usernumber: "",
+};
+
+const ContactForm = ({ onAddContact }) => {
+  const userNameId = useId();
+  const userNumberId = useId();
+
+  const handleSubmit = (values, actions) => {
+    onAddContact(values);
+    actions.resetForm();
+  };
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      onSubmit={handleSubmit}
+      validationSchema={ContactFormSchema}
+    >
+      <Form className={css.form}>
+        <label className={css.label} htmlFor={userNameId}>
+          Name
+        </label>
+        <Field
+          className={css.input}
+          type="text"
+          name="username"
+          id={userNameId}
+        ></Field>
+        <ErrorMessage component="span" name="username" />
+        <label className={css.label} htmlFor={userNumberId}>
+          Number
+        </label>
+        <Field
+          className={css.input}
+          type="text"
+          name="usernumber"
+          id={userNumberId}
+        ></Field>
+        <ErrorMessage component="span" name="usernumber" />
+        <button type="submit">Add contact</button>
+      </Form>
+    </Formik>
+  );
 };
 
 export default ContactForm;
